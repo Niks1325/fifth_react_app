6b1c2b77855bbbe62555bbb2726a67ce1eefa943
Nikhil Paramane <nikhilparamane13@gmail.com>
2023-06-16 14:42:24 +0530

    react5

--- fifth-react-app/src/App.js
+++ fifth-react-app/src/App.js
@@ -1,42 +1,14 @@
 import { useRef, useState } from "react";
+import MyTodo from "./pages/MyTodo";
+import MyRegistration from "./pages/MyRegistration";
+//import MyRegistration from "./pages/MyRegistration";
 
 function App() {
   return (
     <>
-      <h1>Stateful List</h1>
-      <ListDemo />
-    </>
-  );
-}
-
-function ListDemo() {
-  let inputRef = useRef();
-  let [list, setList] = useState(["delhi"]);
-
-  let addItemAction = () => {
-    // console.log(inputRef.current);
-    // console.log(document.querySelector("#id1"));
-
-    let inputValue = inputRef.current.value;
-    let newList = [inputValue, ...list];
-    setList(newList);
-
-    inputRef.current.value = "";
-  };
-
-  return (
-    <>
-      <input
-        type="text"
-        id="id1"
-        ref={inputRef}
-        placeholder="Enter user input..."
-      />
-      <input type="button" value="Add New Item" onClick={addItemAction} />
-
-      {list.map((item) => (
-        <h1>{item}</h1>
-      ))}
+      <MyTodo />
+      <hr />
+      <MyRegistration />
     </>
   );
 }
